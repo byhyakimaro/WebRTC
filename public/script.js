547a9013b0b222d7da0937aa4a977bf60650d7b2
hyakimaro <>
2021-04-21 04:57:56 +0000

    OK

--- public/script.js
+++ public/script.js
@@ -59,6 +59,6 @@ function addVideoStream(video, stream) {
     video.play()
   })
   videoGrid.append(video)
-	var tagVideo = document.getElementsByTagName("video").length;
-	video.style.width = (100 / tagVideo)+"%";
+	var tagVideo = document.getElementsByTagName("video");
+	video.style.width = (100 / tagVideo.length)+"%";
 }
