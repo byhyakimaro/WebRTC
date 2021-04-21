87743efa8136c69de3937d444770170e1eb49653
hyakimaro <>
2021-04-21 00:02:05 +0000

    OK

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const io = require('socket.io')(server)
 var ExpressPeerServer = require('peer').ExpressPeerServer;
 
 var options = {
-    debug: true
+  debug: true
 }
 
 //app.use('/peerjs', ExpressPeerServer(server, options));
@@ -14,42 +14,40 @@ peerServer = ExpressPeerServer(server, options)
 app.use('/peerjs', peerServer);
 
 peerServer.on('connection', function(id) {
-    console.log(id)
+  console.log(id)
   console.log(server._clients)
 });
-
 
 const { v4: uuidV4 } = require('uuid')
 app.set('view engine','ejs')
 app.use(express.static('public'))
 
 app.get('/', (req,res) => {
-    res.render('home')
+  res.render('home')
 })
 
 app.get('/create', (req,res) =>{
-    res.redirect(`/room/${uuidV4()}`)
-   // res.redirect(`/room/roomid`)
+  res.redirect(`/room/${uuidV4()}`)
+  // res.redirect(`/room/roomid`)
 })
 
 app.get('/room/:room',(req, res) =>{
-    if(validator.isUUID(req.params.room))
-        res.render('room',{roomId: req.params.room})
-    else
-        res.render('home',{error: "Invalid Room ID " + req.params.room})
+  if(validator.isUUID(req.params.room))
+    res.render('room',{roomId: req.params.room})
+  else
+    res.render('home',{error: "Invalid Room ID " + req.params.room})
  
 })
 
 io.on('connection', socket => {
-    socket.on('join-room', (roomId, userId) => {
-        socket.join(roomId)
-        socket.to(roomId).broadcast.emit('user-connected',userId)
-        console.log(roomId, userId);
-        socket.on('disconnect', () => {
-            socket.to(roomId).broadcast.emit('user-disconnected', userId)
-        })
+  socket.on('join-room', (roomId, userId) => {
+    socket.join(roomId)
+    socket.to(roomId).broadcast.emit('user-connected',userId)
+    console.log(roomId, userId);
+    socket.on('disconnect', () => {
+      socket.to(roomId).broadcast.emit('user-disconnected', userId)
     })
+  })
 })
-
 
 server.listen(3000)
